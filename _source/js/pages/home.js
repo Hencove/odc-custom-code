4a46818547adaaa6c07e56be33c74435a6250e80
Jonathan Bell <jonathan@hencove.com>
2025-04-14 17:24:02 -0400

    Create initial random bubbling code for people section

--- _source/js/pages/home.js
+++ _source/js/pages/home.js
@@ -1,1 +1,22 @@
-console.log("Testing github scripts");
+document.addEventListener("DOMContentLoaded", (event) => {
+  let peopleItems = document.querySelectorAll(".home_people_collection-item-container");
+  const xRange = 10;
+  const yRange = 1;
+  const sizeRangeMax = 9;
+  const sizeRangeMin = 6;
+  
+  for (const peopleItem of peopleItems) {
+	  let size = getRandomNum(sizeRangeMin, sizeRangeMax);
+	  let xPos = getRandomNum(0, xRange);
+	  let yPos = getRandomNum(0, yRange);
+	  
+	  peopleItem.style.translate = xPos + "rem " + yPos + "rem ";
+	  peopleItem.style.width = size + "rem";
+	  peopleItem.style.height = size + "rem";
+	  
+  }
+});
+
+function getRandomNum(min, max) {
+  return Math.random() * (max - min) + min;
+}
