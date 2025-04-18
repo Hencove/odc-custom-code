32447d539514e0d70ccaf47f4902347616a14c28
Jonathan Bell <jonathan@hencove.com>
2025-04-18 15:27:15 -0400

    Move grid randomization to a function

--- _source/js/pages/home.js
+++ _source/js/pages/home.js
@@ -1,24 +1,29 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  let peopleItems = document.querySelectorAll(".home_people_collection-item-container");
+  randomizePeopleGrid();
+});
+
+function randomizePeopleGrid() {
+  let peopleItems = document.querySelectorAll(
+    ".home_people_collection-item-container",
+  );
   const xRange = 12;
   const yRange = 1;
   const sizeRangeMax = 9;
   const sizeRangeMin = 6;
-  
+
   for (const peopleItem of peopleItems) {
-	  let size = getRandomNum(sizeRangeMin, sizeRangeMax);
-	  let xPos = getRandomNum(0, xRange);
-	  let yPos = getRandomNum(-1 * yRange, yRange);
-	  let spacer = peopleItem.querySelector(".home_people_popover-spacer");
-	  
-	  peopleItem.style.translate = xPos + "rem " + yPos + "rem ";
-	  peopleItem.style.width = size + "rem";
-	  peopleItem.style.height = size + "rem";
-	  
-	  spacer.style.height = size + "rem";
-	  
+    let size = getRandomNum(sizeRangeMin, sizeRangeMax);
+    let xPos = getRandomNum(0, xRange);
+    let yPos = getRandomNum(-1 * yRange, yRange);
+    let spacer = peopleItem.querySelector(".home_people_popover-spacer");
+
+    peopleItem.style.translate = xPos + "rem " + yPos + "rem ";
+    peopleItem.style.width = size + "rem";
+    peopleItem.style.height = size + "rem";
+
+    spacer.style.height = size + "rem";
   }
-});
+}
 
 function getRandomNum(min, max) {
   return Math.random() * (max - min) + min;
